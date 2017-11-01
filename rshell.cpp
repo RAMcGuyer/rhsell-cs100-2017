f2ab90ebd1494c6efe06858d5bad1833e14b4555
Sean Doerr <sdoer001@hammer.cs.ucr.edu>
2017-10-31 22:23:31 -0700

    Successfully parsed multiple commands and stored them into a vector

--- rshell.cpp
+++ rshell.cpp
@@ -1,23 +1,30 @@
 #include "rshell.h"
 #include <boost/tokenizer.hpp>
+#include <boost/algorithm/string/trim.hpp>
 using namespace boost;
 
 int main()
 {
     string input;
+    vector<string> inputs;
 
     cout << "Enter your command: ";
     getline(cin, input);
 
     typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
-    boost::char_separator<char> sep("&&", "||", boost::keep_empty_tokens);
+    boost::char_separator<char> sep("&&||;");
     tokenizer tokens(input, sep);
 
     for (tokenizer::iterator tok_iter = tokens.begin();
          tok_iter != tokens.end(); ++tok_iter)
     {
-        cout << *tok_iter;
-        cout << "\n";
+        inputs.push_back(*tok_iter);
+    }
+
+    for(int i = 0; i < inputs.size(); i++)
+    {
+        trim(inputs[i]);
+        cout << "Index [" << i << "]: " << inputs[i] << endl;
     }
 
 }
