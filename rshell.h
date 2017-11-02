ee5c71f9ab3570ccca3e82677993da9c33b67bc3
Ross McGuyer <rmcgu002@hammer.cs.ucr.edu>
2017-11-01 19:47:47 -0700

    sorry about that

--- rshell.h
+++ rshell.h
@@ -4,6 +4,15 @@
 #include <iostream>
 #include <cstring>
 #include <string>
+#include <boost/tokenizer.hpp>
+#include <boost/algorithm/string/trim.hpp>
+#include <queue>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
 using namespace std;
 
 #endif
